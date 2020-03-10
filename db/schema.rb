7a1ef5f7804f26ba55ee85e37f41e3c811368a1e
Yuuki883 <yuukisaitou14@gmail.com>
2020-03-11 01:40:36 +0900

    #26 update
    上記と同じ。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_195857) do
+ActiveRecord::Schema.define(version: 2020_03_10_072453) do
 
   create_table "post_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "post_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_01_195857) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "place_name"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
