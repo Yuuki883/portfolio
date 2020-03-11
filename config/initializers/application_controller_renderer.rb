b2be114039d3a666329b1d8f9aa211e1ffab739a
Yuuki883 <yuukisaitou14@gmail.com>
2020-03-11 18:30:30 +0900

    #17 update
    画像投稿のため、refaileのsecret_key追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7ff1296e95f29b30b09bb660aeda0a9e027844e4d73b0fc4697fce5dda3fb72396797439cbb7e4f9b816cb3f22fdc4fc6edf7a794ef3af71aaa86fd7e4b9214f'
