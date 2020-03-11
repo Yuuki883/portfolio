769450771d737388397a537a85b16f74b16038ed
Yuuki883 <yuukisaitou14@gmail.com>
2020-03-11 18:29:01 +0900

    #17 update usersコントローラーのupdateアクション修正 バリテーション 内容変更 editページの修正

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,22 +1,36 @@
 class Users::UsersController < ApplicationController
 
-def index
-   @user = User.find(current_user.id)
-end#Ex:- add_index("admin_users", "username")
+   def index
+      @user = User.find(current_user.id)
+   end#Ex:- add_index("admin_users", "username")
 
-def show
-   @user = User.find(params[:id]) #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
-   @posts = @user.posts
-   @post = Post.new
-end
+   def show
+      @user = User.find(params[:id]) #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+      @posts = @user.posts
+      @post = Post.new
+   end
 
- def edit
-	@user = User.find(params[:id])
-end
+   def edit
+      @user = User.find(params[:id])
+   end
 
-  end
+   def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+         redirect_to user_path(@user.id), notice: "successfully updated your profile!"
+      else
+         render "edit"
+      end
+   end
 
-  private
-  def user_params
-   params.require(:user).permit(:name, :introduction, :profile_image)
+   private
+   def user_params
+      params.require(:user).permit(:name, :introduce, :image, :like_busho, :like_age)
+   end
+
+   def baria_user
+      unless params[:id].to_i == current_user.id
+         redirect_to user_path(current_user)
+      end
+   end
 end
