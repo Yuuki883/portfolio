7cf3081460eb8b2cb75a98d8977bbd3abacec7a3
Yuuki883 <yuukisaitou14@gmail.com>
2020-04-17 15:58:49 +0900

    #9 update
    Userモデルのテストコード追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     name { Faker::Lorem.characters(number:20) }
     email { Faker::Internet.email }
     introduction { Faker::Lorem.characters(number:100) }
+    like_age { Faker::Lorem.characters(number:100) }
+    like_busho { Faker::Lorem.characters(number:100) }
     password { 'password' }
     password_confirmation { 'password' }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,28 +10,72 @@ RSpec.describe 'Userモデルのテスト', type: :model do
         test_user.name = ''
         is_expected.to eq false;
       end
-      it '1文字以上であること' do
+      it 'nameが存在しないので保存されない' do
+        user = User.new(email: 'aaa@aaaa', password: '111111')
+        expect(user).not_to be_valid
+      end
+      it 'nameが1文字以上であること' do
         test_user.name = Faker::Lorem.characters(number:0)
         is_expected.to eq false;
       end
-      it '20文字以下であること' do
-        test_user.name = Faker::Lorem.characters(number:20)
+      it 'nameが20文字以下であること' do
+        test_user.name = Faker::Lorem.characters(number:21)
         is_expected.to eq false;
       end
     end
 
     context 'introductionカラム' do
       let(:test_user) { user }
-      it '100文字以下であること' do
-        test_user.introduction = Faker::Lorem.characters(number:100)
+      it 'introductionが100文字以下であること' do
+        test_user.introduction = Faker::Lorem.characters(number:101)
+        is_expected.to eq false
+      end
+    end
+    context 'like_ageカラム' do
+      let(:test_user) { user }
+      it 'like_ageが100文字以下であること' do
+        test_user.introduction = Faker::Lorem.characters(number:101)
+        is_expected.to eq false
+      end
+    end
+    context 'like_bushoカラム' do
+      let(:test_user) { user }
+      it 'like_bushoが100文字以下であること' do
+        test_user.introduction = Faker::Lorem.characters(number:101)
         is_expected.to eq false
       end
     end
   end
+
   describe 'アソシエーションのテスト' do
     context 'Postモデルとの関係' do
       it '1:Nとなっている' do
         expect(User.reflect_on_association(:posts).macro).to eq :has_many
+      end
+    end
+    context 'Topicモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:topics).macro).to eq :has_many
+      end
+    end
+    context 'Topic_commentsモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:topic_comments).macro).to eq :has_many
+      end
+    end
+    context 'Messagesモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:messages).macro).to eq :has_many
+      end
+    end
+    context 'Favoritesモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:favorites).macro).to eq :has_many
+      end
+    end
+    context 'Entriesモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:entries).macro).to eq :has_many
       end
     end
   end
