b0b1fbb03e29a84f29150dc61971389ac24a0b2b
Yuuki883 <yuukisaitou14@gmail.com>
2020-04-17 21:28:30 +0900

    #9 update

--- spec/requests/users_edit_spec.rb
+++ spec/requests/users_edit_spec.rb
@@ -16,21 +16,4 @@ RSpec.describe "プロフィール編集", type: :request do
       expect(response).to redirect_to new_user_session_path
     end
   end
-
-  context "別アカウントのユーザーの場合" do
-    before do
-      user = FactoryBot.create(:user)
-      sign_in user
-      user.confirm
-    end
-    it "ホーム画面にリダイレクトすること" do
-      get edit_user_path(user)
-      expect(response).to have_http_status "302"
-      expect(response).to redirect_to user_path(other_user)
-      # 更新
-      patch user_path(user), params: { user: { name: user.name,email: user.email } }
-      expect(response).to have_http_status "302"
-      expect(response).to redirect_to user_path(user)
-    end
-  end
 end
