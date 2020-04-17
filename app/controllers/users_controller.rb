4271ef18aef38cd1d1ad623744bdc1c91b3590cb
Yuuki883 <yuukisaitou14@gmail.com>
2020-04-17 21:35:14 +0900

    #17 update
    before_action :authenticate_user!の内容修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :show, :edit, :update, :destroy, :following, :followers]
+  before_action :authenticate_user!
   before_action :baria_user, only: [:edit, :update]
 
   def index
