53eec558fbae40d911b30579069d293e8b6bad67
Yuuki883 <yuukisaitou14@gmail.com>
2020-04-21 02:42:56 +0900

    #26 update
    ページネーションの表示数変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def index
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).page(params[:page]).per(5)
+    @posts = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
   def show
